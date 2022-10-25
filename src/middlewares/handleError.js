e3bb9b60a067ab78286df2847297b5539545242d
Danilo Martins <dansm86@outlook.com>
2022-10-25 19:51:09 -0300

    created errorMiddleware and custom error class

---
+++ src/middlewares/handleError.js
@@ -0,0 +1,10 @@
+const { CustomAPIError } = require('../errors/customError');
+
+const errorHandler = (err, _req, res, _next) => {
+  if (err instanceof CustomAPIError) {
+    return res.status(err.statusCode).json({ message: err.message });
+  }
+  res.status(500).json({ message: 'Database error, please try again' });
+};
+
+module.exports = errorHandler;
