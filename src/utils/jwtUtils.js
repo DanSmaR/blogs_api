a3aaea6ae01a00f7e73b306a6d11d6b5324044ab
Danilo Martins <dansm86@outlook.com>
2022-10-25 22:35:29 -0300

    created jwt utils functions

---
+++ src/utils/jwtUtils.js
@@ -0,0 +1,25 @@
+require('dotenv/config');
+const jwt = require('jsonwebtoken');
+const { createCustomError } = require('../errors/customError');
+
+const createToken = (data) => {
+  const token = jwt.sign({ data }, process.env.JWT_SECRET, {
+    expiresIn: '10m',
+    algorithm: 'HS256',
+  });
+  return token;
+};
+
+const validateToken = (token) => {
+  try {
+    const { data } = jwt.verify(token, process.env.JWT_SECRET);
+    return data;
+  } catch (err) {
+    throw createCustomError('Expired or invalid token', 401);
+  }
+};
+
+module.exports = {
+  createToken,
+  validateToken,
+};
