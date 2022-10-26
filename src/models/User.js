f7eab9b748b58e872791c0d6677b0f1388d2cd0a
Danilo Martins <dansm86@outlook.com>
2022-10-26 18:03:10 -0300

    req10-created BlogPost model and associations

--- src/models/User.js
+++ src/models/User.js
@@ -1,13 +1,24 @@
-module.exports = (sequelize, DataTypes) => sequelize.define('User', {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-  },
-  displayName: DataTypes.STRING,
-  email: DataTypes.STRING,
-  password: DataTypes.STRING,
-  image: DataTypes.STRING,
-}, {
-  underscored: true,
-  timestamps: false,
-});
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  }, {
+    underscored: true,
+    timestamps: false,
+  });
+
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost, {
+      foreignKey: 'user_id',
+      as: 'blog_posts',
+    })
+  }
+
+  return User;
+}
